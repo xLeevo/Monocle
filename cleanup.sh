a2290d3c2c376f3fa440b03047825132a768bca8
Monkey Ted Baker <monkey.ted.baker@gmail.com>
2017-07-14 01:14:40 +0000

    [add]cleanup scripts

--- cleanup.sh
+++ cleanup.sh
@@ -1,1 +1,2 @@
 mysql -h $DYNAMO_SOURCEDB_MONOCLE -u pgonet -p$POGOMAP_DB_PASS pgonetdb_monocle < /var/www/Monocle/sql/cleanup.sql
+#mysql -h $DYNAMO_SOURCEDB_MONOCLE -u pgonet -p$POGOMAP_DB_PASS pgonetdb_monocle < /var/www/Monocle/sql/cleanup_sightings.sql
