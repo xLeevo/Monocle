ecb3c79d56938f4dc76bbfef42bab939b36ea2e6
Monkey Ted Baker <monkey.ted.baker@gmail.com>
2017-07-19 03:56:32 +0000

    [add]cleanup_spawnpoints [add]deeplinks

---
+++ cleanup_spawnpoints.sh
@@ -0,0 +1,2 @@
+mysql -h $DYNAMO_SOURCEDB_MONOCLE -u pgonet -p$POGOMAP_DB_PASS pgonetdb_monocle < /var/www/Monocle/sql/cleanup_spawnpoints.sql
+#mysql -h $DYNAMO_SOURCEDB_MONOCLE -u pgonet -p$POGOMAP_DB_PASS pgonetdb_monocle < /var/www/Monocle/sql/cleanup_sightings.sql
