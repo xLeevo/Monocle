4b2c84a96885a21723585861c6e336b5059dcb56
Hydro74000 <33062049+Hydro74000@users.noreply.github.com>
2017-11-08 13:45:34 +0100

    Deleting GO_HASH references for using ZeChrales aiopogo.

--- scan.py
+++ scan.py
@@ -208,14 +208,7 @@ def main():
     overseer.start(args.status_bar)
     launcher = LOOP.create_task(overseer.launch(args.bootstrap, args.pickle))
     
-    if conf.GO_HASH:
-        hashkey = conf.GO_HASH_KEY
-    else:
-        hashkey = conf.HASH_KEY
-    activate_hash_server(hashkey,
-            go_hash=conf.GO_HASH,
-            hash_endpoint=conf.HASH_ENDPOINT,
-            gohash_endpoint=conf.GOHASH_ENDPOINT)
+    activate_hash_server(conf.HASH_KEY)
     if platform != 'win32':
         LOOP.add_signal_handler(SIGINT, launcher.cancel)
         LOOP.add_signal_handler(SIGTERM, launcher.cancel)
