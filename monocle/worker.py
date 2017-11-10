4ec142656f70d3d19e6f4b2d1ded4a58f0437702
Falaris01 <weulejulian@gmail.com>
2017-11-10 20:38:05 +0100

    79.4

--- monocle/worker.py
+++ monocle/worker.py
@@ -255,7 +255,7 @@ class Worker:
             raise err
 
         self.error_code = '°'
-        version = 7903
+        version = 7904
         async with self.sim_semaphore:
             self.error_code = 'APP SIMULATION'
             if conf.APP_SIMULATION:
@@ -680,9 +680,9 @@ class Worker:
             else:
                 if (not dl_hash
                         and conf.FORCED_KILL
-                        and dl_settings.settings.minimum_client_version != '0.79.3'):
+                        and dl_settings.settings.minimum_client_version != '0.79.4'):
                     forced_version = StrictVersion(dl_settings.settings.minimum_client_version)
-                    if forced_version > StrictVersion('0.79.3'):
+                    if forced_version > StrictVersion('0.79.4'):
                         err = '{} is being forced, exiting.'.format(forced_version)
                         self.log.error(err)
                         print(err)
