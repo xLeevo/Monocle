96556ec0a55c6d32f0013d923268000028f7f199
Hydro74000 <acymap@gmail.com>
2017-11-30 15:25:41 -0500

    Update to 0.83.2 for kill

--- monocle/worker.py
+++ monocle/worker.py
@@ -680,9 +680,9 @@ class Worker:
             else:
                 if (not dl_hash
                         and conf.FORCED_KILL
-                        and dl_settings.settings.minimum_client_version != '0.83.0'):
+                        and dl_settings.settings.minimum_client_version != '0.83.2'):
                     forced_version = StrictVersion(dl_settings.settings.minimum_client_version)
-                    if forced_version > StrictVersion('0.83.0'):
+                    if forced_version > StrictVersion('0.83.2'):
                         err = '{} is being forced, exiting.'.format(forced_version)
                         self.log.error(err)
                         print(err)
