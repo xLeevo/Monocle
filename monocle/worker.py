7f052cf5bdbfacc00ff4e5afe0d5effd9cb07680
Hydro74000 <acymap@gmail.com>
2017-12-08 13:37:35 -0500

    Updated forced version change to 85.1

--- monocle/worker.py
+++ monocle/worker.py
@@ -257,7 +257,7 @@ class Worker:
             raise err
 
         self.error_code = '°'
-        version = 8300
+        version = 8501
         async with self.sim_semaphore:
             self.error_code = 'APP SIMULATION'
             if conf.APP_SIMULATION:
@@ -684,9 +684,9 @@ class Worker:
             else:
                 if (not dl_hash
                         and conf.FORCED_KILL
-                        and dl_settings.settings.minimum_client_version != '0.83.2'):
+                        and dl_settings.settings.minimum_client_version != '0.85.1'):
                     forced_version = StrictVersion(dl_settings.settings.minimum_client_version)
-                    if forced_version > StrictVersion('0.83.2'):
+                    if forced_version > StrictVersion('0.85.1'):
                         err = '{} is being forced, exiting.'.format(forced_version)
                         self.log.error(err)
                         print(err)
